507397957dd1b79634fc24555a9313a0ab310017
chonghuo <chobg.huo@tcredit.com>
2020-10-27 22:22:52 +0800

    提交20201027

--- src/components/contents/tabControl/TabControl.vue
+++ src/components/contents/tabControl/TabControl.vue
@@ -1,16 +1,62 @@
 <template>
   <div class="tab-control">
-    <slot></slot>
+    <!--当仅有文字不一样的时候就不用插槽了-->
+    <div v-for="(item,index) in titles" class="tab-control-item"
+         :class="{active: index==currentIndex}"
+         @click="tabClick(index)">
+      <span>{{item}}</span>
+    </div>
   </div>
 
 </template>
 
 <script>
 export default {
-name: "TabControl"
+  name: "TabControl",
+  props:{
+    titles:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  data(){
+    return{
+      currentIndex:0
+    }
+  },
+  methods:{
+    tabClick(index){
+      this.currentIndex = index
+    }
+  }
 }
 </script>
 
 <style scoped>
+  .tab-control{
+    display: flex;
+    text-align: center;
+    background-color: #fff;
 
+  }
+  .tab-control-item{
+    flex: 1;
+    /*设置文字高度*/
+    line-height:40px;
+    /*设置span的高度*/
+    height: 40px;
+  }
+  .tab-control-item span{
+    /*设置vid 和 span之间的宽度*/
+    padding: 5px;
+  }
+  .active{
+    color: var(--color-high-text);
+  }
+  .active span{
+    /*下划线*/
+    border-bottom: 3px solid var(--color-high-text);
+  }
 </style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -6,15 +6,79 @@
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend :recommends="recommends"></home-recommend>
     <home-feature></home-feature>
+    <tab-control :titles="['流行','新款','精选',]" class="tab-control"></tab-control>
+    <li>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+      <ul>li</ul>
+    </li>
   </div>
 </template>
 
 <script>
     import NavBar from "@/components/common/nabbar/NavBar";
+
     import HomeSwiper from "@/views/home/childComps/HomeSwiper";
     import HomeRecommend from "@/views/home/childComps/HomeRecommend";
     import HomeFeature from "@/views/home/childComps/HomeFeature";
-    import {getHomeMutidata} from "@/network/home";
+
+    import TabControl from "@/components/contents/tabControl/TabControl";
+
+    import {getHomeMutidata,getHomeGoods} from "@/network/home";
 
 
     export default {
@@ -23,24 +87,46 @@
           HomeFeature,
           HomeSwiper,
           NavBar,
-          HomeRecommend
+          HomeRecommend,
+          TabControl
         },
         data(){
           return{
               banners:[],
-              recommends:[]
+              recommends:[],
+              goods:{
+                'pop':{page:0,list:[]},
+                'new':{page:0,list:[]},
+                'sell':{page:0,list:[]}
+              }
           }
         },
         /*在组件创建完毕后就发送请求*/
         created() {
           //1、请求多个数据
+          this.getHomeMutidata()
+          //2、请求商品数据
+          this.getHomeGoods('pop')
+          this.getHomeGoods('new')
+          this.getHomeGoods('sell')
+        },
+      methods:{
+        getHomeMutidata(){
           getHomeMutidata().then(res=>{
             // res 是一个局部变量，需要将返回数据存在data中
             this.banners = res.data.banner.list
             this.recommends = res.data.recommend.list
             console.log(this.banners)
           })
+        },
+        getHomeGoods(type){
+          const page = this.goods[type].page + 1
+          getHomeGoods(type,page).then(res=>{
+            this.goods[type].list.push(...res.data.list)
+            this.goods[type].page += 1
+          })
         }
+      }
     }
 </script>
 
@@ -82,7 +168,10 @@
     right: 0;
     left: 0;
   }
-
+  .tab-control{
+    position: sticky;
+    top:44px;
+  }
 
 
 </style>
