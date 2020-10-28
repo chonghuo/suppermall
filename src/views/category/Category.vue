b591333f51fa73080ac674a3c418bcf8eef44ba6
Chong.Huo <Chong.Huo@cicc.com.cn>
2020-10-28 18:23:19 +0800

    '2020102818'

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,13 +1,101 @@
 <template>
-  <h2>分类</h2>
+  <div class="wrapper" >
+    <ul class="content">
+      <li>121</li>
+      <li>122</li>
+      <li>123</li>
+      <li>124</li>
+      <li>125</li>
+      <li>126</li>
+      <li>172</li>
+      <li>128</li>
+      <li>126</li>
+      <li>152</li>
+      <li>132</li>
+      <li>12w</li>
+      <li>1w2</li>
+      <li>132</li>
+      <li>132</li>
+      <li>124</li>
+      <li>12e</li>
+      <li>1w2</li>
+      <li>1e2</li>
+      <li>12w</li>
+      <li>1s2</li>
+      <li>s12</li>
+      <li>1s2</li>
+      <li>1e2</li>
+      <li>12f</li>
+      <li>125</li>
+      <li>152</li>
+      <li>1f2</li>
+      <li>121</li>
+      <li>112</li>
+      <li>1h2</li>
+      <li>1s2</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
+    import BScroll from 'better-scroll'
     export default {
-        name: "Category"
+        name: "Category",
+        mounted() {
+           new BScroll(document.querySelector('.wrapper'),{
+          })
+        }
     }
 </script>
 
 <style scoped>
-
+  .wrapper{
+    height: 150px;
+    background-color: red;
+  }
 </style>
